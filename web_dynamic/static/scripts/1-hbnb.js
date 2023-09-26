80a3838230c1d2cd21d1bf4bab272fe6929572b1
lenne sebastien <sebastienlenne@gmail.com>
2023-09-26 09:42:58 +0200

    add file 1-hbnb.js for task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,34 @@
-$(document).ready(function() {
-  var amenityIDs = {};
+$(document).ready(function () {
+  // Initialisez une liste vide pour stocker les aménités cochées
+  var selectedAmenities = [];
 
-  $('input[type="checkbox"]').change(function() {
-      var amenityID = $(this).data('id');
+  // Sélectionnez toutes les cases à cocher
+  $('input[type="checkbox"]').change(function () {
+      var amenityId = $(this).data('id');
+      var amenityName = $(this).data('name');
 
-      if ($(this).prop('checked')) {
-          amenityIDs[amenityID] = true;
+      if ($(this).is(':checked')) {
+          // Si la case à cocher est cochée, ajoutez l'aménité à la liste
+          selectedAmenities.push({ id: amenityId, name: amenityName });
       } else {
-          delete amenityIDs[amenityID];
+          // Si la case à cocher est décochée, retirez l'aménité de la liste
+          selectedAmenities = selectedAmenities.filter(function (amenity) {
+              return amenity.id !== amenityId;
+          });
       }
 
-      var selectedAmenities = Object.keys(amenityIDs).join(', ');
-      $('.amenities h4').text(selectedAmenities);
+      // Mettez à jour la balise h4 avec la liste des aménités cochées
+      var amenityList = selectedAmenities.map(function (amenity) {
+          return amenity.name;
+      }).join(', ');
+
+      $('.amenities h4').text('Amenities: ' + amenityList);
   });
+
+  // Vous pouvez également initialiser la balise h4 avec la liste d'aménités actuellement cochées
+  var amenityList = selectedAmenities.map(function (amenity) {
+      return amenity.name;
+  }).join(', ');
+
+  $('.amenities h4').text('Amenities: ' + amenityList);
 });
