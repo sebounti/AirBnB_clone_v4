05cdcb483a986963aa8327ee7ee12854821d410f
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-26 16:05:15 +0200

    modify header css and api

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,40 +1,28 @@
-$(document).ready(function () {
-    // Initialisez une liste vide pour stocker les aménités cochées
-    let selectedAmenities = [];
-  
-    // Sélectionnez toutes les cases à cocher avec la classe "checkbox-amenities"
-    $('.checkbox-amenities').change(function () {
-      var amenityId = $(this).data('id');
-      var amenityName = $(this).data('name');
-  
-      if ($(this).is(':checked')) {
-        // Si la case à cocher est cochée, ajoutez l'aménité à la liste
-        selectedAmenities.push({ id: amenityId, name: amenityName });
-      } else {
-        // Si la case à cocher est décochée, retirez l'aménité de la liste
-        selectedAmenities = selectedAmenities.filter(function (amenity) {
-          return amenity.id !== amenityId;
-        });
-      }
-  
-      // Mettez à jour la balise h4 avec la liste des aménités cochées
-      var amenityList = selectedAmenities.map(function (amenity) {
-        return amenity.name;
-      }).join(', ');
-  
-      $('.amenities h4').text('Amenities: ' + amenityList);
-    });
-  });
-
-$(document).ready(function () {
+$('document').ready(function () {
     // requête GET vers l'URL de l'API
-    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-        if (data.status === 'OK') {
-            // Si le statut est OK, ajoutez la classe "available" à div#api_status
-            $('#api_status').addClass('available');
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+        // Si le statut est OK, ajoutez la classe "available"
+        $('#api_status').addClass('available');
+    } else {
+        // Sinon, supprimez la classe "available" (pour que le bouton passe au gris)
+        $('#api_status').removeClass('available');
+    }
+});
+
+    //display check box on each item
+    let amenities = {};
+    $('INPUT[type="checkbox"]').change(function () {
+        if ($(this).is(':checked')) {
+            amenities[$(this).attr('data-id')] = $(this).attr('data-name');
         } else {
-            // Sinon, supprimez la classe "available"
-            $('#api_status').removeClass('available');
+            delete amenities[$(this).attr('data-id')];
+        }
+        if (Object.values(amenities).length === 0) {
+            $('.amenities H4').html('&nbsp;');
+        } else {
+            $('.amenities H4').text(Object.values(amenities).join(', '));
         }
     });
+
 });
