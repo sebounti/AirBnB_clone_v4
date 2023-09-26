42b6682ff0d7fb3c27e0f470269081ae2d4162f0
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-26 11:29:24 +0200

    Check box in html and js

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -37,12 +37,13 @@ def hbnb():
 
     cache_id = str(uuid.uuid4())
 
-    return render_template('0-hbnb.html',
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
                            cache_id=cache_id
                            )
+
 
 if __name__ == "__main__":
     """ Main Function """
--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,34 +1,28 @@
 $(document).ready(function () {
-  // Initialisez une liste vide pour stocker les aménités cochées
-  var selectedAmenities = [];
-
-  // Sélectionnez toutes les cases à cocher
-  $('input[type="checkbox"]').change(function () {
+    // Initialisez une liste vide pour stocker les aménités cochées
+    let selectedAmenities = [];
+  
+    // Sélectionnez toutes les cases à cocher avec la classe "checkbox-amenities"
+    $('.checkbox-amenities').change(function () {
       var amenityId = $(this).data('id');
       var amenityName = $(this).data('name');
-
+  
       if ($(this).is(':checked')) {
-          // Si la case à cocher est cochée, ajoutez l'aménité à la liste
-          selectedAmenities.push({ id: amenityId, name: amenityName });
+        // Si la case à cocher est cochée, ajoutez l'aménité à la liste
+        selectedAmenities.push({ id: amenityId, name: amenityName });
       } else {
-          // Si la case à cocher est décochée, retirez l'aménité de la liste
-          selectedAmenities = selectedAmenities.filter(function (amenity) {
-              return amenity.id !== amenityId;
-          });
+        // Si la case à cocher est décochée, retirez l'aménité de la liste
+        selectedAmenities = selectedAmenities.filter(function (amenity) {
+          return amenity.id !== amenityId;
+        });
       }
-
+  
       // Mettez à jour la balise h4 avec la liste des aménités cochées
       var amenityList = selectedAmenities.map(function (amenity) {
-          return amenity.name;
+        return amenity.name;
       }).join(', ');
-
+  
       $('.amenities h4').text('Amenities: ' + amenityList);
+    });
   });
-
-  // Vous pouvez également initialiser la balise h4 avec la liste d'aménités actuellement cochées
-  var amenityList = selectedAmenities.map(function (amenity) {
-      return amenity.name;
-  }).join(', ');
-
-  $('.amenities h4').text('Amenities: ' + amenityList);
-});
+  
